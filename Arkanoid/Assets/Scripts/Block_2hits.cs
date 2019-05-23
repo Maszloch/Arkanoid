35293582a2d84e6c34d1aefa2b2ce6be14892835
Maszloch <izabelamaszloch@gmail.com>
2019-05-23 03:39:55 +0200

    Dodano nowy skrypt (bloki niszczone po dwóch uderzeniach). Jeszcze brak zmieniających się sprite

--- Arkanoid/Assets/Scripts/Block_2hits.cs
+++ Arkanoid/Assets/Scripts/Block_2hits.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class Block_2hits : MonoBehaviour
 {
     public Rigidbody2D ball;
+    public Sprite Block_Broken_1;
+    public Sprite Block_Broken_2;
+    
+
+    public int hitsNeeded = 2;
+    public int hitsTaken;
+
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        hitsTaken += 1;
         ball.velocity = new Vector2(ball.velocity.x, -ball.velocity.y);
+
+        if (hitsTaken >= hitsNeeded)
+        {
+            Destroy(gameObject);
+        }
+        
+        
     }
 }
